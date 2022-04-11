d11ca84ffa19a11157d051fe5d0f2bf6df206f31
kshitij-23 <kshitijsolanki23@gmail.com>
2022-04-11 09:23:04 +0530

    Added all services

---
+++ withdraw-deposit/src/main/java/com/ksh/withdrawdeposit/WithdrawDepositApplication.java
@@ -0,0 +1,13 @@
+package com.ksh.withdrawdeposit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithdrawDepositApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithdrawDepositApplication.class, args);
+	}
+
+}
---
+++ withdraw-deposit/src/test/java/com/ksh/withdrawdeposit/WithdrawDepositApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksh.withdrawdeposit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithdrawDepositApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
